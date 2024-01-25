eb0cdbd078f7a694f43ba7d33c9de795dfb7be22
humbertoluks <luks.dev@outlook.com>
2024-01-25 07:32:54 -0300

    propriedades e alguns estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import { Header } from './Components/Header';
 import Post from './Post';
+import './global.css'
 
 function App() {
   return (
-  
     <div>
+      <Header />
       <Post 
         author="Humberto Luksevicius" 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda sit similique eos laudantium maiores? Maxime ipsum, necessitatibus sed quidem tenetur rem eos esse voluptatem. Assumenda obcaecati ipsam voluptas ea facere?"
